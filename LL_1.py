685392230277272b2880fe802e502c611d7fa67b
Aleksandra Daņilova <144416304+davanilla@users.noreply.github.com>
2025-02-18 23:29:21 +0200

    231RWB008 Task 2 done

--- LL_1.py
+++ LL_1.py
@@ -1,41 +1,56 @@
+##St.apl.Nr: 231RWB008 Aleksandra Daņilova RDBI0 1. grupa
 class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
         
-
 class LinkedList:
     def __init__(self, value):
         new_node = Node(value)
         self.head = new_node
         self.tail = new_node
+        self.length = 1
 
-        
     def append(self, value):
         new_node = Node(value)
-        if self.head == None:
+        if self.length == 0:
             self.head = new_node
             self.tail = new_node
         else:
             self.tail.next = new_node
             self.tail = new_node
+        self.length += 1
         return True
-        
 
-    # WRITE FIND_MIDDLE_NODE METHOD HERE #
-    #                                    #
-    #                                    #
-    #                                    #
-    #                                    #
-    ######################################
+    def has_loop(self):
+        slow = self.head
+        fast = self.head
+
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+            
+        return False
+
+    
+my_linked_list_1 = LinkedList(1)
+my_linked_list_1.append(2)
+my_linked_list_1.append(3)
+my_linked_list_1.append(4)
+my_linked_list_1.tail.next = my_linked_list_1.head
+print(my_linked_list_1.has_loop() ) # Returns True
 
 
 
-my_linked_list = LinkedList(1)
-my_linked_list.append(2)
-my_linked_list.append(3)
-my_linked_list.append(4)
-my_linked_list.append(5)
 
-print( my_linked_list.find_middle_node().value )
+my_linked_list_2 = LinkedList(1)
+my_linked_list_2.append(2)
+my_linked_list_2.append(3)
+my_linked_list_2.append(4)
+print(my_linked_list_2.has_loop() ) # Returns False
+
+
 
